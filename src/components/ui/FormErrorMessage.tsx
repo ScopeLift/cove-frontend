be107dc38243120062c005ad1039b0ebe00a88b3
Keating <keating.dev@protonmail.com>
2023-06-12 16:51:48 -0400

    Remove turnary

--- src/components/ui/FormErrorMessage.tsx
+++ src/components/ui/FormErrorMessage.tsx
@@ -1,10 +1,10 @@
 const FormErrorMessage = ({ error }: { error?: string }) => {
-  return error ? (
-    <p role='alert' className='mt-1 text-xs text-red-600'>
-      {error}
-    </p>
-  ) : (
-    <></>
+  return (
+    (error && (
+      <p role='alert' className='mt-1 text-xs text-red-600'>
+        {error}
+      </p>
+    )) || <></>
   );
 };
 
