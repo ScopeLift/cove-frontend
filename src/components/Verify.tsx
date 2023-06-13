397b977618922ce19de64c0787ab9807c5311d09
Keating <keating.dev@protonmail.com>
2023-06-12 21:19:10 -0400

    Fix build

--- src/components/Verify.tsx
+++ src/components/Verify.tsx
@@ -36,7 +36,7 @@ export const Verify = () => {
       creationTxHashes: [
         {
           chainId: SUPPORTED_CHAINS.mainnet.id,
-          hash: '',
+          hash: '' as `0x{string}`,
         },
       ],
     },
@@ -235,7 +235,7 @@ export const Verify = () => {
               className='mt-8 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
               onClick={() => {
                 setSelectedChains((prev) => [...prev, SUPPORTED_CHAINS.mainnet]);
-                append({ chainId: SUPPORTED_CHAINS.mainnet.id, hash: '' });
+                append({ chainId: SUPPORTED_CHAINS.mainnet.id, hash: '' as `0x{string}` });
               }}
             >
               Add another chain
