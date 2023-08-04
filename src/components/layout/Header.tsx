1c7abd6e97612cf8f96ae0fdc268d6b6a9b6d01a
Matt Solomon <matt@mattsolomon.dev>
2023-08-04 11:32:14 -0700

    feat: add signup banner

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,29 +1,26 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import logo from 'public/logo.png';
+import { ExternalLink } from '@/components/layout/ExternalLink';
 import { SITE_NAME } from '@/lib/constants';
 
 export const Header = () => {
-  const currentPath = useRouter().pathname;
-
-  const NavLink = (props: { path: string; label: string }) => {
-    const activeClass = props.path === currentPath ? 'font-bold' : '';
-    return (
-      <Link href={props.path} className={`text-base font-medium text-gray-500 ${activeClass}`}>
-        {props.label}
-      </Link>
-    );
-  };
-
   return (
-    <div className='flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10'>
-      <div>
-        <Link href='/' className='flex items-center'>
-          <Image className='h-12 w-auto p-1 sm:h-16' src={logo} alt='logo' />
-          <span className='sr-only ml-4'>{SITE_NAME}</span>
-        </Link>
+    <>
+      <div className='bg-secondary gap-x-6 px-6 py-2.5 text-center shadow-sm sm:px-3.5 sm:before:flex-1'>
+        <p className='text-sm font-bold leading-6'>
+          <ExternalLink href='https://www.scopelift.co/cove' text='Sign up' /> to stay up to date
+          with Cove.
+        </p>
       </div>
-    </div>
+      <div className='flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10'>
+        <div>
+          <Link href='/' className='flex items-center'>
+            <Image className='h-12 w-auto p-1 sm:h-16' src={logo} alt='logo' />
+            <span className='sr-only ml-4'>{SITE_NAME}</span>
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
