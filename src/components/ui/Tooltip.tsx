1218ed169594b2398a8ce89a802a219ffdc22945
wildmolasses <changes@gmail.com>
2023-08-06 12:14:25 -0500

    tooltip fix

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -4,7 +4,7 @@ export const Tooltip = ({ text, width = 24 }: { text: string; width?: number }) 
   return (
     <div className='group relative'>
       <InformationCircleIcon width={width} />
-      <span className='bg-secondary absolute top-10 scale-0 rounded border border-gray-300 p-2 text-xs group-hover:scale-100 dark:border-gray-900'>
+      <span className='bg-secondary absolute -right-20 top-10 z-50 w-56 scale-0 rounded border border-gray-300 p-2 text-xs group-hover:scale-100 dark:border-gray-900'>
         {text}
       </span>
     </div>
