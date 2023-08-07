0a48283c329c3d8e620ae27725c20f7f7fc826d2
Matt Solomon <matt@mattsolomon.dev>
2023-08-07 15:20:49 -0700

    chore: fix typo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const SITE_NAME = 'Cove';
 export const SITE_DESCRIPTION = 'Robust, open-source contract verification for the EVM';
 export const COMPANY_NAME = 'ScopeLift';
 export const COMPANY_URL = 'http://covecontracts.com/';
-export const GITHUB_URL = 'https://github.com/ScpopeLift/cove-backend';
+export const GITHUB_URL = 'https://github.com/ScopeLift/cove-backend';
 export const TWITTER_URL = 'https://twitter.com/ScopeLift';
 
 export const IS_DEV_MODE = process.env.NODE_ENV === 'development';
